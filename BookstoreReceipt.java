bc06592f4bb32cd4cf025e9208c28c56c264b1ef
Jonathan Chuang <115677967+chuangjonathan123@users.noreply.github.com>
2025-09-28 15:17:29 -0700

    Add files via upload

---
+++ BookstoreReceipt.java
@@ -0,0 +1,25 @@
+public class BookstoreReceipt
+{
+    private String customerName;
+    private int numberOfBooks;
+    private double pricePerBook;
+    private String thankYouMessage;
+    
+    public BookstoreReceipt(String name, int num, String message)
+    {
+        customerName = name;
+        numberOfBooks = num;
+        pricePerBook = 13.99;
+        thankYouMessage = message; 
+    }
+    
+    //Complete this method that returns a String of the receipt information
+    public String getReceipt()
+    {
+        String build = "Customer Name: " + customerName;
+        build += "\nNumber of Books: " + numberOfBooks;
+        build += "\nTotal Price: $" + pricePerBook * numberOfBooks;
+        build += "\n" + thankYouMessage;
+        return build;
+    }
+}
