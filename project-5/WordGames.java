62ff397ecfddc35d370b79d02cdd4513c7f14f87
Jonathan Chuang <115677967+chuangjonathan123@users.noreply.github.com>
2025-10-01 21:06:03 -0700

    Update WordGames.java

--- project-5/WordGames.java
+++ project-5/WordGames.java
@@ -3,20 +3,17 @@ public class WordGames
     
     public static String scramble(String word)
     {
-        // switch first half
         int middle = word.length() / 2;
         String left = word.substring(0,middle);
         String right = word.substring(middle);
         return right + left;
-        // and second half
       
     }
     
     
     public static String bananaSplit(String word, int insertIdx, String insertText)
     {
-        // Insert insertText at the position
-        // insertIdx
+       
         String left = word.substring(0,insertIdx);
         String right = word.substring (insertIdx);
         return left +insertText + right;
@@ -26,8 +23,7 @@ public class WordGames
     
     public static String bananaSplit(String word, String insertChar, String insertText)
     {
-        // Insert insertText after the first
-        // occurence of the insertChar
+        
         int insertIdx = word.indexOf(insertChar);
         String left = word.substring(0, insertIdx);
         String right = word.substring(insertIdx);
