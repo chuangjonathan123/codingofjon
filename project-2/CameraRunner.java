4c4e7d7f40f714590dc8519264391b05c7b5bf7d
Jonathan Chuang <115677967+chuangjonathan123@users.noreply.github.com>
2025-10-01 21:04:30 -0700

    Update CameraRunner.java

--- project-2/CameraRunner.java
+++ project-2/CameraRunner.java
@@ -2,7 +2,6 @@ public class CameraRunner
 {
     public static void main(String[] args) 
     {
-       // Create at least 3 camera objects and print them out
        Camera one = new Camera("film", 10);
        Camera two = new Camera(3, 100);
        Camera three = new Camera("digital", 100);
