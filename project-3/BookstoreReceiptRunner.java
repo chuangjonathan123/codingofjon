85a3901ec7bfb2c7ff8891a3ba681d1ac36ac976
Jonathan Chuang <115677967+chuangjonathan123@users.noreply.github.com>
2025-10-01 21:04:54 -0700

    Update BookstoreReceiptRunner.java

--- project-3/BookstoreReceiptRunner.java
+++ project-3/BookstoreReceiptRunner.java
@@ -2,10 +2,8 @@ public class BookstoreReceiptRunner
 {
     public static void main(String[] args)
     {
-        //Create 2 BookstoreReceipt objects
-        BookstoreReceipt jon = new BookstoreReceipt("JOnathan", 49, "Thanks for shopping!");
+        BookstoreReceipt jon = new BookstoreReceipt("Jonathan", 49, "Thanks for shopping!");
         BookstoreReceipt stan = new BookstoreReceipt("harry potter", 4, "Thanks for shopping!");
-        //Print out the receipts
         System.out.println(jon.getReceipt());
         System.out.println(stan.getReceipt());
     }
