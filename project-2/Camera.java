0fe15015891cc319f0b1eebfaa0348b5862382a7
Jonathan Chuang <115677967+chuangjonathan123@users.noreply.github.com>
2025-10-01 21:04:20 -0700

    Update Camera.java

--- project-2/Camera.java
+++ project-2/Camera.java
@@ -1,11 +1,9 @@
 public class Camera 
 {
-    // Attributes    
     private String filmFormat;
     private int megaPixels;
     private int maxZoom;
 
-    // Constructor with film format and zoom inputs
     public Camera(String format, int zoom) 
     {
         filmFormat = format;
@@ -13,7 +11,6 @@ public class Camera
         megaPixels = 0; 
     }
 
-    // Constructor with pixels and zoom inputs
     public Camera(int pixels, int zoom) 
     {
         megaPixels = pixels;
@@ -21,7 +18,6 @@ public class Camera
         filmFormat = null;
     }
 
-    // Overriding method that prints only the max zoom
     public String toString() 
     {
         return "This is a camera with a maxiumum zoom of : " + maxZoom;
