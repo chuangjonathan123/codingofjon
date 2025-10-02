f6b8b4ec59c8736e4278e99b97d1f14029488397
Jonathan Chuang <115677967+chuangjonathan123@users.noreply.github.com>
2025-10-01 21:02:25 -0700

    Update BottleRunner.java

--- project-1/BottleRunner.java
+++ project-1/BottleRunner.java
@@ -2,9 +2,7 @@ public class BottleRunner
 {
     public static void main(String[] args) 
     {
-        // Create a water bottle object and use the instance
-        // methods and a print statement to match the desired
-        // output:
+       
         WaterBottle jon = new WaterBottle(1.0, 0.6);
         jon.drink(0.4);
         jon.drink(0.4);
