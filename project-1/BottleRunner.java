d3d01c09b288cdb0c1acef97f0c00c8c75aa0382
Jonathan Chuang <115677967+chuangjonathan123@users.noreply.github.com>
2025-10-02 17:30:19 -0700

    Update BottleRunner.java

--- project-1/BottleRunner.java
+++ project-1/BottleRunner.java
@@ -2,7 +2,7 @@ public class BottleRunner
 {
     public static void main(String[] args) 
     {
-       
+        // making a math model on how many sips it would take for a refill to be necesary
         WaterBottle jon = new WaterBottle(1.0, 0.6);
         jon.drink(0.4);
         jon.drink(0.4);
