d4a8e7c414113cbe5d0699669ee3d664e50c7a96
Jonathan Chuang <115677967+chuangjonathan123@users.noreply.github.com>
2025-10-08 16:11:59 -0700

    Update FRC code 2024.java

--- FRC project-/FRC code 2024.java
+++ FRC project-/FRC code 2024.java
@@ -1,3 +1,4 @@
+//dug up some code from last year
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
@@ -10,7 +11,7 @@ public class Robot extends TimedRobot {
     private PWMSparkMax rightDriveMotor = new PWMSparkMax(1);
     private Timer timer = new Timer();
 
-    private Joystick joystick = new Joystick(0); // Make sure you actually have this plugged in
+    private Joystick joystick = new Joystick(0); 
 
     @Override
     public void robotInit() {
@@ -27,22 +28,18 @@ public class Robot extends TimedRobot {
     public void autonomousPeriodic() {
         double time = timer.get();
 
-        // Drive forward for 2 seconds
         if (time < 2.0) {
             leftDriveMotor.set(0.5);
             rightDriveMotor.set(0.5);
         } 
-        // Turn right for 1 second
         else if (time < 3.0) {
             leftDriveMotor.set(0.5);
             rightDriveMotor.set(-0.5);
         }
-        // Drive backward for 2 seconds
         else if (time < 5.0) {
             leftDriveMotor.set(-0.5);
             rightDriveMotor.set(-0.5);
         } 
-        // Stop
         else {
             leftDriveMotor.set(0.0);
             rightDriveMotor.set(0.0);
@@ -51,7 +48,7 @@ public class Robot extends TimedRobot {
 
     @Override
     public void teleopPeriodic() {
-        double forward = -joystick.getY(); // usually forward is negative Y
+        double forward = -joystick.getY(); 
         double turn = joystick.getX();
 
         double leftPower = forward + turn;
